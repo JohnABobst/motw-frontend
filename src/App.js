31f6c544a0f01c8875314a95b6418388591115f9
John Bobst <johnabobst@gmail.com>
2024-03-29 21:31:30 -0700

    Created components for minimum functionality.  Displaying character info, logging in, logging out, giving input to the chatbot, and displaying output.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Keeper from './components/Keeper/Keeper';
+import React, { useState } from 'react';
+import Campaign from './components/Campaign/Campaign';
+import Authorization from './components/Authorization/Authorization';
+import Header from './components/Header/Header';
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const dummyPlayers = [
+    { name: 'Player 1', online: true },
+    { name: 'Player 2', online: false },
+    { name: 'Player 3', online: true },
+    { name: 'Player 4', online: false },
+    { name: 'Player 5', online: true },
+  ];
+
+  
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  }
+
   return (
-    <div className="App">
-      <header className="Monster of the Week">
-      </header>
-      <Keeper />
+    <div className="app-container">
+      <main className="app-main">
+      <Header handleLogout={handleLogout} players={dummyPlayers} />
+        {isLoggedIn ? (
+          <Campaign />
+        ) : (
+          <Authorization
+            handleLogin={handleLogin}
+          />
+        )}
+      </main>
     </div>
   );
 }
