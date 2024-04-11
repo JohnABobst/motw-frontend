c0ef735c6bc6d563563991aeefa3bbf90018310c
John Bobst <johnabobst@gmail.com>
2024-04-11 15:24:52 -0700

    fixed positioning issues in header and modals (#2)

--- src/components/Header/PlayersList/PlayersList.js
+++ src/components/Header/PlayersList/PlayersList.js
@@ -3,14 +3,12 @@ import './PlayersList.css'
 
 const PlayersList = ({ players }) => {
   return (
-    <div className="player-list">
-            <div className='player-list-container'>
-            {players.map((player, index) => (
-          <div key={index} className={`player-item ${player.online ? 'online' : 'offline'}`}>
-            {player.name}
-          </div>
-        ))}
-            </div>
+    <div className='player-list-container'>
+      {players.map((player, index) => (
+        <div key={index} className={`player-item ${player.online ? 'online' : 'offline'}`}>
+          {player.name}
+        </div>
+      ))}
     </div>
   );
 };
